42797d6e6e3e436212e9dc973c54623844eceaa8
lxymahatma <lxymahatma@gmail.com>
2025-10-16 18:25:03 +0900

    better organize

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,8 +1,21 @@
 local config = {}
 
 config.colorscheme = "catppuccin"
-config.enabled_langs = require("helpers.lang_loader").get_enabled_languages()
 
-require("config.lazy").setup(config.enabled_langs)
+local default_langs = {
+    "git",
+    "json",
+    "lua",
+    "markdown",
+    "nushell",
+    "toml",
+    "xml",
+    "yaml",
+}
+
+local extra_langs = require("helpers.lang_loader").get_extra_langs()
+config.enabled_langs = vim.list_extend(default_langs, extra_langs)
+
 require("config.options")
+require("config.lazy").setup(config.enabled_langs)
 require("config.colorschemes").setup(config.colorscheme)
