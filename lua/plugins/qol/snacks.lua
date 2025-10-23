f02d8f6ab4e2737d0d638569d3a71e109c025391
lxymahatma <lxymahatma@gmail.com>
2025-10-23 21:05:01 +0900

    flash integration with snacks

--- lua/plugins/qol/snacks.lua
+++ lua/plugins/qol/snacks.lua
@@ -62,6 +62,32 @@ return {
                     hidden = true,
                 },
             },
+            win = {
+                input = {
+                    keys = {
+                        ["<M-s>"] = { "flash", mode = { "n", "i" } },
+                        ["s"] = { "flash" },
+                    },
+                },
+            },
+            actions = {
+                flash = function(picker)
+                    require("flash").jump({
+                        pattern = "^",
+                        label = { after = { 0, 0 } },
+                        search = {
+                            mode = "search",
+                            exclude = {
+                                function(win) return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list" end,
+                            },
+                        },
+                        action = function(match)
+                            local idx = picker.list:row2idx(match.pos[1])
+                            picker.list:_move(idx, true, true)
+                        end,
+                    })
+                end,
+            },
         },
         quickfile = { enabled = true },
         scroll = { enabled = true },
