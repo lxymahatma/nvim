274a7d15d12beae37f7563ec8e80d8b3cb837e4b
lxymahatma <lxymahatma@gmail.com>
2025-10-26 01:48:46 +0900

    finish linting config

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -1,14 +1,25 @@
 return {
-    -- TODO: Finish Configuration
     {
         "mfussenegger/nvim-lint",
         event = "BufEdit",
         init = function()
-            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-                callback = function()
+            local events = { "BufReadPost", "BufWritePost", "InsertLeave" }
+            local function debounce(ms, fn)
+                local timer = vim.uv.new_timer()
+                return function()
+                    timer:start(ms, 0, function()
+                        timer:stop()
+                        vim.schedule_wrap(fn)()
+                    end)
+                end
+            end
+
+            vim.api.nvim_create_autocmd(events, {
+                group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+                callback = debounce(100, function()
                     require("lint").try_lint()
                     require("lint").try_lint("cspell")
-                end,
+                end),
             })
         end,
     },
