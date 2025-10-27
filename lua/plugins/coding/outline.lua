607a9449f82069071bc7f688bd1b91d6a1cc2a25
lxymahatma <lxymahatma@gmail.com>
2025-10-27 18:08:17 +0900

    use aerial not outline

--- lua/plugins/coding/outline.lua
+++
@@ -1,9 +0,0 @@
--- Outline
-return {
-    "hedyhli/outline.nvim",
-    cmd = { "Outline", "OutlineOpen" },
-    keys = {
-        { "<leader>to", "<cmd>Outline<CR>", desc = "Toggle outline" },
-    },
-    opts = {},
-}
---
+++ lua/plugins/qol/aerial.lua
@@ -0,0 +1,33 @@
+return {
+    "stevearc/aerial.nvim",
+    event = "BufEdit",
+    opts = {
+        autojump = true,
+        attach_mode = "global",
+        backends = { "lsp", "treesitter", "markdown", "asciidoc", "man" },
+        lazy_load = true,
+        layout = {
+            min_width = 0.3,
+            max_width = 0.4,
+        },
+        filter_kind = {
+            "Class",
+            "Constructor",
+            "Enum",
+            "Field",
+            "Function",
+            "Interface",
+            "Method",
+            "Module",
+            "Namespace",
+            "Package",
+            "Property",
+            "Struct",
+            "Trait",
+        },
+        show_guides = true,
+    },
+    keys = {
+        { "<leader>cs", "<cmd>AerialToggle<cr>", desc = "Aerial Toggle" },
+    },
+}
