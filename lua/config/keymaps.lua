c9846b70958c8390476a6f593e3203947bee9b30
lxymahatma <lxymahatma@gmail.com>
2025-10-30 18:20:24 +0900

    add window split & close keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,9 @@ map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
 map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+map("n", "<leader>wj", "<C-w>s", { desc = "Split Window Below", remap = true })
+map("n", "<leader>wl", "<C-w>v", { desc = "Split Window Right", remap = true })
+map("n", "<leader>wc", "<C-w>c", { desc = "Close Current Window", remap = true })
 
 -- Insert mode, ctrl + h / ctrl + l move to the beginning and end of the line
 map("i", "<C-h>", "<HOME>", { desc = "Go to the beginning of the line", remap = true })
