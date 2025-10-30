3ebe945ac139c7fc4aafa7ac02b581d73191835e
lxymahatma <lxymahatma@gmail.com>
2025-10-30 17:25:50 +0900

    little more lualine config

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -7,7 +7,10 @@ return {
         options = {
             theme = "auto",
             globalstatus = true,
-            disabled_filetypes = { statusline = { "snacks_dashboard" } },
+            disabled_filetypes = {
+                statusline = { "snacks_dashboard" },
+                winbar = { "snacks_dashboard" },
+            },
         },
         sections = {
             lualine_a = { "mode" },
@@ -25,6 +28,22 @@ return {
             lualine_x = { "overseer", "encoding", "fileformat", "filetype" },
             lualine_y = { "progress" },
             lualine_z = { "location" },
+        },
+        winbar = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { "filename" },
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = {},
+        },
+        inactive_winbar = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { "filename" },
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = {},
         },
         extensions = { "lazy", "man", "mason", "overseer", "trouble" },
     },
