b59dd27469062ccf323643373969b1d8c70062a2
lxymahatma <lxymahatma@gmail.com>
2025-11-01 03:13:59 +0900

    fix autotag config

--- lua/plugins/editor/ts-autotag.lua
+++ lua/plugins/editor/ts-autotag.lua
@@ -2,10 +2,12 @@ return {
     "windwp/nvim-ts-autotag",
     event = "BufEdit",
 
-    ---@type nvim-ts-autotag.Opts
     opts = {
-        enable_close = true,
-        enable_rename = true,
-        enable_close_on_slash = true,
+        ---@type nvim-ts-autotag.Opts
+        opts = {
+            enable_close = true,
+            enable_rename = true,
+            enable_close_on_slash = true,
+        },
     },
 }
