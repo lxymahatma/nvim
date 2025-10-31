cc07bad112a1786e345ca954fefc83ef5033b57b
lxymahatma <lxymahatma@gmail.com>
2025-11-01 01:49:42 +0900

    add opts for nvim-ts-autotag

--- lua/plugins/editor/ts-autotag.lua
+++ lua/plugins/editor/ts-autotag.lua
@@ -2,6 +2,10 @@ return {
     "windwp/nvim-ts-autotag",
     event = "BufEdit",
 
-    ---@type nvim-ts-autotag.PluginSetup
-    opts = {},
+    ---@type nvim-ts-autotag.Opts
+    opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = true,
+    },
 }
