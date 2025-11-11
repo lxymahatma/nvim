70c2a6f4d8da21f05b611a4e1840c11b89be4e79
lxymahatma <lxymahatma@gmail.com>
2025-11-11 23:29:05 +0900

    add kdl

---
+++ lua/plugins/lang/kdl.lua
@@ -0,0 +1,28 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "kdl",
+            },
+        },
+    },
+
+    {
+        "mason-org/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "kdlfmt",
+            },
+        },
+    },
+
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                kdl = { "kdlfmt" },
+            },
+        },
+    },
+}
