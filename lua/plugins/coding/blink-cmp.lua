7dfc0befa1414870e8c13b48725ac9a8521b22a7
lxymahatma <lxymahatma@gmail.com>
2025-11-13 15:44:25 +0900

    move copilot hide menu into copilot.lua not blink cmp

--- lua/plugins/coding/blink-cmp.lua
+++ lua/plugins/coding/blink-cmp.lua
@@ -118,18 +118,5 @@ return {
                 },
             })
         end,
-        config = function(_, opts)
-            require("blink.cmp").setup(opts)
-
-            vim.api.nvim_create_autocmd("User", {
-                pattern = "BlinkCmpMenuOpen",
-                callback = function() vim.b.copilot_suggestion_hidden = true end,
-            })
-
-            vim.api.nvim_create_autocmd("User", {
-                pattern = "BlinkCmpMenuClose",
-                callback = function() vim.b.copilot_suggestion_hidden = false end,
-            })
-        end,
     },
 }
--- lua/plugins/coding/copilot.lua
+++ lua/plugins/coding/copilot.lua
@@ -21,4 +21,17 @@ return {
             enabled = false,
         },
     },
+    config = function(_, opts)
+        require("copilot").setup(opts)
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "BlinkCmpMenuOpen",
+            callback = function() vim.b.copilot_suggestion_hidden = true end,
+        })
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "BlinkCmpMenuClose",
+            callback = function() vim.b.copilot_suggestion_hidden = false end,
+        })
+    end,
 }
