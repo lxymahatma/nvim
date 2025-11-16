cf13ad3ab2060e1f09fc30d5590845e037bece94
lxymahatma <lxymahatma@gmail.com>
2025-11-17 01:42:50 +0900

    add qmlformat

--- lua/plugins/lang/qml.lua
+++ lua/plugins/lang/qml.lua
@@ -25,4 +25,13 @@ return {
             },
         },
     },
+
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                qml = { "qmlformat" },
+            },
+        },
+    },
 }
