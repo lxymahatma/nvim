58ad8c47c7c9991f7e9c4e26217a96c4aa6954e8
lxymahatma <lxymahatma@gmail.com>
2025-11-17 01:38:24 +0900

    add close buffer methods

---
+++ lua/helpers/buffer.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+function M.close_buffers(direction)
+    local current = vim.api.nvim_get_current_buf()
+    local current_idx, buffers = nil, {}
+
+    for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_get_option_value("buflisted", { buf = bufnr }) then
+            buffers[#buffers + 1] = bufnr
+            if bufnr == current then current_idx = #buffers end
+        end
+    end
+
+    if not current_idx then return end
+
+    if direction == "left" then
+        for i = current_idx - 1, 1, -1 do
+            vim.api.nvim_buf_delete(buffers[i], { force = false })
+        end
+    elseif direction == "right" then
+        for i = current_idx + 1, #buffers do
+            vim.api.nvim_buf_delete(buffers[i], { force = false })
+        end
+    end
+end
+
+return M
