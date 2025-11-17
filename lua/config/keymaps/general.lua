19e45c71bc208731fdb8e8f27fc88c4f7d487ec2
lxymahatma <lxymahatma@gmail.com>
2025-11-18 00:36:35 +0900

    buffer delete keymaps

--- lua/config/keymaps/general.lua
+++ lua/config/keymaps/general.lua
@@ -4,7 +4,8 @@ local del = Snacks.keymap.del
 -- Buffer Keymaps
 map("n", "H", "<cmd>bprevious<cr>", { desc = "Previous Buffer", remap = true })
 map("n", "L", "<cmd>bnext<cr>", { desc = "Next Buffer", remap = true })
-map("n", "<leader>bd", function() Snacks.bufdelete() end, { desc = "Delete buffer", remap = true })
+map("n", "<leader>bd", Snacks.bufdelete.delete, { desc = "Delete current buffer", remap = true })
+map("n", "<leader>bo", Snacks.bufdelete.other, { desc = "Delete other buffers", remap = true })
 map("n", "<leader>bl", function() require("helpers.buffer").close_buffers("left") end, { desc = "Close buffers on the left", remap = true })
 map("n", "<leader>br", function() require("helpers.buffer").close_buffers("right") end, { desc = "Close buffers on the right", remap = true })
 
