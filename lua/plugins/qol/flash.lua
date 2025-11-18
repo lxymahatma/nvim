a85cb0921fdd468eed0a03d9f5142bdbdb9abcd4
lxymahatma <lxymahatma@gmail.com>
2025-11-18 20:26:54 +0900

    use s and S for flash because mini surround map s to <nop>

--- lua/plugins/qol/flash.lua
+++ lua/plugins/qol/flash.lua
@@ -10,8 +10,8 @@ return {
         },
     },
     keys = {
-        { "gs",    function() require("flash").jump() end,              desc = "Flash",               mode = { "n", "x", "o" } },
-        { "gS",    function() require("flash").treesitter() end,        desc = "Flash Treesitter",    mode = { "n", "x", "o" } },
+        { "s",     function() require("flash").jump() end,              desc = "Flash",               mode = { "n", "x", "o" } },
+        { "S",     function() require("flash").treesitter() end,        desc = "Flash Treesitter",    mode = { "n", "x", "o" } },
         { "r",     function() require("flash").remote() end,            desc = "Remote Flash",        mode = "o" },
         { "R",     function() require("flash").treesitter_search() end, desc = "Treesitter Search",   mode = { "o", "x" } },
         { "<C-s>", function() require("flash").toggle() end,            desc = "Toggle Flash Search", mode = { "c" } },
