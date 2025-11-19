b1601d9feab9a54fa92b4ffaa92ade7a1ac40094
lxymahatma <lxymahatma@gmail.com>
2025-11-19 20:49:30 +0900

    move inline diagnostic autocmd into own plugin

--- lua/plugins/ui/tiny-inline-diagnostic.lua
+++ lua/plugins/ui/tiny-inline-diagnostic.lua
@@ -22,4 +22,27 @@ return {
             },
         },
     },
+    config = function(_, opts)
+        local tiny_inline = require("tiny-inline-diagnostic")
+        tiny_inline.setup(opts)
+
+        local disabled = false
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "SidekickNesHide",
+            callback = function()
+                if disabled then
+                    disabled = false
+                    tiny_inline.enable()
+                end
+            end,
+        })
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "SidekickNesShow",
+            callback = function()
+                disabled = true
+                tiny_inline.disable()
+            end,
+        })
+    end,
 }
