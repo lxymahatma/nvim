18a6b2480040f3f89d4fc51cc73550834f2d27d5
lxymahatma <lxymahatma@gmail.com>
2025-11-20 00:59:26 +0900

    use vim.o not vim.opt

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,48 +1,49 @@
-local opt = vim.opt
 local o = vim.o
 
-opt.autowrite = true -- Enable auto write
+o.autowrite = true -- Enable auto write
 
 -- Enable relative line numbers
-opt.number = true
-opt.relativenumber = true
+o.number = true
+o.relativenumber = true
 
-opt.clipboard = "unnamedplus" -- Use system clipboard
-opt.ignorecase = true         -- Ignore case
-opt.smartcase = true          -- Don't ignore case with capitals
+o.clipboard = "unnamedplus" -- Use system clipboard
+o.ignorecase = true         -- Ignore case
+o.smartcase = true          -- Don't ignore case with capitals
 
 -- Indent
-opt.smartindent = true -- Insert indents automatically
-opt.shiftround = true  -- Round indent
-opt.shiftwidth = 2     -- Size of an indent
-opt.expandtab = true   -- Use spaces instead of tabs
-opt.tabstop = 2        -- Number of spaces tabs count for
+o.smartindent = true -- Insert indents automatically
+o.shiftround = true  -- Round indent
+o.shiftwidth = 2     -- Size of an indent
+o.expandtab = true   -- Use spaces instead of tabs
+o.tabstop = 2        -- Number of spaces tabs count for
 
-opt.cursorline = true  -- Highlight current line
-opt.mouse = "a"        -- Enable mouse
-opt.confirm = true     -- Confirm to save changes before exiting modified buffer
-opt.showmode = false   -- Don't show mode since we have a statusline
+o.cursorline = true  -- Highlight current line
+o.mouse = "a"        -- Enable mouse
+o.confirm = true     -- Confirm to save changes before exiting modified buffer
+o.showmode = false   -- Don't show mode since we have a statusline
 
 -- Split Window from below and right
-opt.splitkeep = "screen"
-opt.splitbelow = true   -- Put new windows below current
-opt.splitright = true   -- Put new windows right of current
+o.splitkeep = "screen"
+o.splitbelow = true   -- Put new windows below current
+o.splitright = true   -- Put new windows right of current
 
-opt.signcolumn = "yes"  -- Always show the signcolumn
-opt.list = true         -- Show some invisible characters
-opt.scrolloff = 4       -- Lines of context
-opt.smoothscroll = true -- Enable smooth scrolling
-opt.jumpoptions = "view"
-opt.laststatus = 3      -- global statusline
-opt.linebreak = true    -- Wrap lines at convenient points
-opt.wrap = false        -- Disable line wrap
+o.signcolumn = "yes"  -- Always show the signcolumn
+o.list = true         -- Show some invisible characters
+o.scrolloff = 4       -- Lines of context
+o.smoothscroll = true -- Enable smooth scrolling
+o.jumpoptions = "view"
+o.laststatus = 3      -- global statusline
+o.linebreak = true    -- Wrap lines at convenient points
+o.wrap = false        -- Disable line wrap
 
 -- Folding
-opt.foldlevel = 99
-opt.foldmethod = "expr"
-opt.foldexpr = "nvim_treesitter#foldexpr()"
+o.foldlevel = 99
+o.foldmethod = "expr"
+o.foldexpr = "nvim_treesitter#foldexpr()"
 
-opt.termguicolors = true -- True color support
-
+-- Tabs
 o.showtabline = 2
+
+o.termguicolors = true -- True color support
+
 o.sessionoptions = "buffers,curdir,folds,help,localoptions,tabpages,terminal,winpos,winsize"
