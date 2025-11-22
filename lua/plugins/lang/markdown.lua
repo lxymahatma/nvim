496d9b71a65ea6b045a34f92ca4b93f27c21b832
lxymahatma <lxymahatma@gmail.com>
2025-11-22 14:46:01 +0900

    format markdown using markdownlint

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -41,4 +41,13 @@ return {
             },
         },
     },
+
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                markdown = { "markdownlint-cli2" },
+            },
+        },
+    },
 }
