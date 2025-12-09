d0cb16d46bbb8e05b81e9d4bf094623619e1df60
lxymahatma <lxymahatma@gmail.com>
2025-12-09 14:43:40 +0900

    basic treesitter implementation

---
+++ lua/helpers/treesitter.lua
@@ -0,0 +1,34 @@
+local M = {}
+
+local ts = require("nvim-treesitter")
+local helpers = require("helpers.table")
+
+M.ensure_parser_installed = function(parser_list)
+    local installed_set = helpers.to_set(ts.get_installed())
+
+    local install_list = {}
+    local update_list = {}
+
+    for _, parser_name in ipairs(parser_list) do
+        if not installed_set[parser_name] then
+            table.insert(install_list, parser_name)
+        else
+            table.insert(update_list, parser_name)
+        end
+    end
+
+    if #install_list > 0 then ts.install(install_list, { force = false, summary = true }) end
+    if #update_list > 0 then ts.update(update_list, { summary = false }) end
+end
+
+M.install_parser = function(parser_name)
+    vim.notify(("[treesitter] Installing parser: %s"):format(parser_name), vim.log.levels.INFO)
+    ts.install(parser_name, { force = false, summary = true })
+end
+
+M.update_parser = function(parser_name)
+    vim.notify(("[treesitter] Updating parser: %s"):format(parser_name), vim.log.levels.INFO)
+    ts.update(parser_name, { summary = false })
+end
+
+return M
--- lua/plugins/editor/treesitter-textobjects.lua
+++ lua/plugins/editor/treesitter-textobjects.lua
@@ -3,6 +3,6 @@
 return {
     "nvim-treesitter/nvim-treesitter-textobjects",
     event = "BufEdit",
-    main = "nvim-treesitter.configs",
+    branch = "main",
     opts = {},
 }
