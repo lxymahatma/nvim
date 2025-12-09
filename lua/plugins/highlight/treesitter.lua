e3bc0310811529e41c10b78ae938c13d544268ea
lxymahatma <lxymahatma@gmail.com>
2025-12-09 14:43:40 +0900

    basic treesitter implementation

--- lua/plugins/highlight/treesitter.lua
+++ lua/plugins/highlight/treesitter.lua
@@ -2,9 +2,8 @@
 ---@type LazyPluginSpec
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    branch = "main",
     event = { "BufEdit", "VeryLazy" },
-    main = "nvim-treesitter.configs",
     build = ":TSUpdate",
     opts_extend = { "ensure_installed" },
     opts = {
@@ -18,4 +17,8 @@ return {
             "vimdoc",
         },
     },
+    config = function(_, opts)
+        require("nvim-treesitter").setup(opts)
+        vim.defer_fn(function() require("helpers.treesitter").ensure_parser_installed(opts.ensure_installed) end, 100)
+    end,
 }
