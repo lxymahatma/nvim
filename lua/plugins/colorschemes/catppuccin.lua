7143a1927cfa305d0ac5d43b9928d645be0b66ba
lxymahatma <lxymahatma@gmail.com>
2025-12-10 17:07:55 +0900

    set tokyomove colorschemes into folder

---
+++ lua/plugins/colorschemes/catppuccin.lua
@@ -0,0 +1,64 @@
+-- catppuccin
+---@type LazyPluginSpec
+return {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true,
+
+    ---@type CatppuccinOptions
+    opts = {
+        flavour = "mocha",
+        integrations = {
+            aerial = true,
+            blink_cmp = {
+                style = "bordered",
+            },
+            copilot_vim = true,
+            dap = true,
+            dap_ui = true,
+            dashboard = true,
+            flash = true,
+            gitsigns = {
+                enabled = true,
+                transparent = false,
+            },
+            grug_far = true,
+            lsp_trouble = true,
+            mason = true,
+            mini = {
+                enabled = true,
+            },
+            native_lsp = {
+                enabled = true,
+                virtual_text = {
+                    errors = { "italic" },
+                    hints = { "italic" },
+                    warnings = { "italic" },
+                    information = { "italic" },
+                    ok = { "italic" },
+                },
+                underlines = {
+                    errors = { "underline" },
+                    hints = { "underline" },
+                    warnings = { "underline" },
+                    information = { "underline" },
+                    ok = { "underline" },
+                },
+                inlay_hints = {
+                    background = true,
+                },
+            },
+            noice = true,
+            notify = true,
+            overseer = true,
+            rainbow_delimiters = true,
+            render_markdown = true,
+            snacks = {
+                enabled = true,
+            },
+            treesitter = true,
+            treesitter_context = true,
+            which_key = true,
+        },
+    },
+}
---
+++ lua/plugins/colorschemes/tokyonight.lua
@@ -0,0 +1,10 @@
+-- tokyonight
+---@type LazyPluginSpec
+return {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    enabled = false,
+
+    ---@type tokyonight.Config
+    opts = {},
+}
