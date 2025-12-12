c95d7a9072bc67341c10809d36f491d24d176eea
lxymahatma <lxymahatma@gmail.com>
2025-12-12 17:40:41 +0900

    fix type warning

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,11 +4,15 @@ return {
     "neovim/nvim-lspconfig",
     event = "BufEdit",
     config = vim.schedule_wrap(function()
-        Snacks.util.lsp.on({ method = "textDocument/inlayHint" }, function(buffer) vim.lsp.inlay_hint.enable(true, { bufnr = buffer }) end)
+        Snacks.util.lsp.on({ method = "textDocument/inlayHint" }, function(buffer)
+            ---@cast buffer integer
+            vim.lsp.inlay_hint.enable(true, { bufnr = buffer })
+        end)
         Snacks.util.lsp.on({ method = "textDocument/codeLens" }, function(buffer)
+            ---@cast buffer integer
             vim.lsp.codelens.refresh({ bufnr = buffer })
             vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave", "BufWritePost" }, {
-                buffer = buffer, --[[@type integer]]
+                buffer = buffer,
                 callback = function() vim.lsp.codelens.refresh({ bufnr = buffer }) end,
             })
         end)
