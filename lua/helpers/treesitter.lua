79da9a6c03d87d3e02d2825e3df5de5947cd93cb
lxymahatma <lxymahatma@gmail.com>
2025-12-14 00:59:59 +0900

    do not check update because it should happen on build

--- lua/helpers/treesitter.lua
+++ lua/helpers/treesitter.lua
@@ -5,20 +5,13 @@ local ts = require("nvim-treesitter")
 ---@param parser_list string[] List of parser names to ensure are installed
 M.ensure_parsers_installed = function(parser_list)
     local installed_set = require("helpers.table").to_set(ts.get_installed())
-
     local install_list = {}
-    local update_list = {}
 
     for _, parser_name in ipairs(parser_list) do
-        if not installed_set[parser_name] then
-            table.insert(install_list, parser_name)
-        else
-            table.insert(update_list, parser_name)
-        end
+        if not installed_set[parser_name] then table.insert(install_list, parser_name) end
     end
 
     if #install_list > 0 then ts.install(install_list, { force = false, summary = true }) end
-    if #update_list > 0 then ts.update(update_list, { summary = false }) end
 end
 
 ---@param parser_name string
