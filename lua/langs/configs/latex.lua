6649edc0e9ddbb565411529aad7795dc5174452b
lxymahatma <lxymahatma@gmail.com>
2025-12-16 00:41:45 +0900

    fix latex

--- lua/langs/configs/latex.lua
+++ lua/langs/configs/latex.lua
@@ -1,11 +1,12 @@
 ---@type LanguageSpec
 return {
+    filetype = "tex",
     treesitter = true,
     mason = "texlab",
     lsp = "texlab",
     plugin = {
         "lervag/vimtex",
-        event = "VeryLazy",
+        ft = "tex",
         init = function() vim.g.vimtex_view_method = "zathura" end,
     },
 }
