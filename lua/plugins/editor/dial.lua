40af78451fdb37cbb95d46b7bc0f296a1ad6063b
lxymahatma <lxymahatma@gmail.com>
2025-12-15 23:55:37 +0900

    dial config

--- lua/plugins/editor/dial.lua
+++ lua/plugins/editor/dial.lua
@@ -1,33 +1,49 @@
 -- Enhance Ctrl-A/X
+
+---@param direction "increment" | "decrement"
+---@param g boolean
+local function dial(direction, g)
+    local is_visual = vim.fn.mode(true):find("^[vV\22]")
+    local mode = (g and "g" or "") .. (is_visual and "visual" or "normal")
+
+    ---@cast direction direction
+    ---@cast mode mode
+    return require("dial.map").manipulate(direction, mode)
+end
+
 ---@type LazyPluginSpec
 return {
     "monaqa/dial.nvim",
-    config = function()
+    opts = function()
         local augend = require("dial.augend")
-        require("dial.config").augends:register_group({
-            default = {
-                augend.integer.alias.decimal_int,
-                augend.integer.alias.hex,
-                augend.constant.alias.bool,
-                augend.constant.alias.Bool,
-                augend.date.alias["%Y/%m/%d"],
-                augend.date.alias["%m/%d/%Y"],
-                augend.date.alias["%Y年%-m月%-d日"],
-                augend.date.new({
-                    pattern = "%Y/%m/%d",
-                    default_kind = "day" --[[@as datekind]],
-                    only_valid = true,
-                    word = false,
-                }),
-                augend.semver.alias.semver,
-                augend.misc.alias.markdown_header,
+
+        return {
+            groups = {
+                default = {
+                    augend.integer.alias.decimal_int,
+                    augend.integer.alias.hex,
+                    augend.constant.alias.bool,
+                    augend.constant.alias.Bool,
+                    augend.date.alias["%Y/%m/%d"],
+                    augend.date.alias["%m/%d/%Y"],
+                    augend.date.alias["%Y年%-m月%-d日"],
+                    augend.date.new({
+                        pattern = "%Y/%m/%d",
+                        default_kind = "day" --[[@as datekind]],
+                        only_valid = true,
+                        word = false,
+                    }),
+                    augend.semver.alias.semver,
+                    augend.misc.alias.markdown_header,
+                },
             },
-        })
+        }
     end,
+    config = function(_, opts) require("dial.config").augends:register_group(opts.groups) end,
     keys = {
-        { "<C-a>",  "<Plug>(dial-increment)",  mode = { "n", "v" } },
-        { "<C-x>",  "<Plug>(dial-decrement)",  mode = { "n", "v" } },
-        { "g<C-a>", "g<Plug>(dial-increment)", mode = { "n", "v" }, remap = true },
-        { "g<C-x>", "g<Plug>(dial-decrement)", mode = { "n", "v" }, remap = true },
+        { "<C-a>",  function() return dial("increment", false) end, desc = "Increment", mode = { "n", "v" } },
+        { "<C-x>",  function() return dial("decrement", false) end, desc = "Decrement", mode = { "n", "v" } },
+        { "g<C-a>", function() return dial("increment", true) end,  desc = "Increment", mode = { "n", "x" } },
+        { "g<C-x>", function() return dial("decrement", true) end,  desc = "Decrement", mode = { "n", "x" } },
     },
 }
