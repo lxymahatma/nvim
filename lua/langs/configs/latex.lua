b3872be460e8972ce58edcf1b5bc19bdfaf487a9
lxymahatma <lxymahatma@gmail.com>
2025-12-16 00:28:50 +0900

    latex plugin

--- lua/langs/configs/latex.lua
+++ lua/langs/configs/latex.lua
@@ -3,4 +3,9 @@ return {
     treesitter = true,
     mason = "texlab",
     lsp = "texlab",
+    plugin = {
+        "lervag/vimtex",
+        event = "VeryLazy",
+        init = function() vim.g.vimtex_view_method = "zathura" end,
+    },
 }
