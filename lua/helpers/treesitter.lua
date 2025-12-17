0ffea72f2a3138ce285b2817c890f698780c1a21
lxymahatma <lxymahatma@gmail.com>
2025-12-17 14:45:11 +0900

    ts.install is no-op if parsers already installed

--- lua/helpers/treesitter.lua
+++ lua/helpers/treesitter.lua
@@ -3,16 +3,7 @@ local M = {}
 local ts = require("nvim-treesitter")
 
 ---@param parser_list string[] List of parser names to ensure are installed
-M.ensure_parsers_installed = function(parser_list)
-    local installed_set = require("helpers.table").to_set(ts.get_installed())
-    local install_list = {}
-
-    for _, parser_name in ipairs(parser_list) do
-        if not installed_set[parser_name] then table.insert(install_list, parser_name) end
-    end
-
-    if #install_list > 0 then ts.install(install_list, { force = false, summary = true }) end
-end
+M.ensure_parsers_installed = function(parser_list) ts.install(parser_list, { force = false, summary = true }) end
 
 ---@param parser_name string
 M.install_parser = function(parser_name)
