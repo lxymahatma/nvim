b6626660e02bba667465c00796dec2770f2bb071
lxymahatma <lxymahatma@gmail.com>
2025-12-21 23:46:07 +0900

    update dial config

--- lua/plugins/editor/dial.lua
+++ lua/plugins/editor/dial.lua
@@ -1,14 +1,21 @@
 -- Enhance Ctrl-A/X
 
+---@type table<string, string> Map of filetype to dial group
+local ft_groups = {
+    lua = "lua",
+    markdown = "markdown",
+}
+
 ---@param direction "increment" | "decrement"
 ---@param g boolean
 local function dial(direction, g)
     local is_visual = vim.fn.mode(true):find("^[vV\22]")
     local mode = (g and "g" or "") .. (is_visual and "visual" or "normal")
+    local group = ft_groups[vim.bo.filetype] or "default"
 
     ---@cast direction direction
     ---@cast mode mode
-    return require("dial.map").manipulate(direction, mode)
+    return require("dial.map").manipulate(direction, mode, group)
 end
 
 ---@type LazyPluginSpec
@@ -34,12 +41,32 @@ return {
                         word = false,
                     }),
                     augend.semver.alias.semver,
+                },
+                lua = {
+                    augend.constant.new({
+                        elements = { "and", "or" },
+                        word = true,
+                        cyclic = true,
+                    }),
+                },
+                markdown = {
+                    augend.constant.new({
+                        elements = { "[ ]", "[x]" },
+                        word = false,
+                        cyclic = true,
+                    }),
                     augend.misc.alias.markdown_header,
                 },
             },
         }
     end,
-    config = function(_, opts) require("dial.config").augends:register_group(opts.groups) end,
+    config = function(_, opts)
+        for name, group in pairs(opts.groups) do
+            if name ~= "default" then vim.list_extend(group, opts.groups.default) end
+        end
+
+        require("dial.config").augends:register_group(opts.groups)
+    end,
     keys = {
         { "<C-a>",  function() return dial("increment", false) end, desc = "Increment", mode = { "n", "v" } },
         { "<C-x>",  function() return dial("decrement", false) end, desc = "Decrement", mode = { "n", "v" } },
