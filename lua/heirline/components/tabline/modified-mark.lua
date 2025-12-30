15bb7cd0d1b0a710383e2eafd385aa80560c7790
lxymahatma <lxymahatma@gmail.com>
2025-12-30 20:26:54 +0900

    use self.modified and put it into init

--- lua/heirline/components/tabline/modified-mark.lua
+++ lua/heirline/components/tabline/modified-mark.lua
@@ -4,6 +4,7 @@ return {
     static = {
         modified_icon = " " .. icons.bufferline.modified,
     },
-    provider = function(self) return vim.api.nvim_get_option_value("modified", { buf = self.bufnr }) and self.modified_icon or "  " end,
+    init = function(self) self.modified = vim.api.nvim_get_option_value("modified", { buf = self.bufnr }) end,
+    provider = function(self) return self.modified and self.modified_icon or "  " end,
     hl = { fg = "orange" },
 }
