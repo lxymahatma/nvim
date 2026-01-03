df676ca4065e4694cb33f5c7cb7cdcedca0f2f63
lxymahatma <lxymahatma@gmail.com>
2026-01-04 04:56:16 +0900

    fix cli icon

--- lua/config/heirline/statusline/section-x.lua
+++ lua/config/heirline/statusline/section-x.lua
@@ -29,7 +29,7 @@ local SidekickCli = {
     condition = function() return #require("sidekick.status").cli() > 0 end,
     init = function(self) self.status = require("sidekick.status").cli() end,
     {
-        provider = function(self) return "  " .. (#self.status > 1 and #self.status or "") end,
+        provider = function(self) return "  " .. (#self.status > 1 and #self.status or "") end,
         hl = { fg = Snacks.util.color("Special") },
     },
     {
