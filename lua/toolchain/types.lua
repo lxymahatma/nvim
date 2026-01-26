6def64a4409af9470fc5a04c25920ec48d393879
lxymahatma <lxymahatma@gmail.com>
2026-01-26 13:21:08 +0800

    some fixes for types

--- lua/toolchain/types.lua
+++ lua/toolchain/types.lua
@@ -6,7 +6,7 @@
 --- @field filetype? string | string[] Filetype(s) associated with the language.
 --- @field mason? MasonPackageSpec | MasonPackageSpec[] Mason packages to install. Can be a single package name/spec or a list of package names/specs.
 --- @field lsp? string | string[] | table<string, vim.lsp.ClientConfig> LSP servers configuration. Can be a single server name (default config) or (server_name -> config) map.
---- @field formatter? string | conform.FiletypeFormatter | table<string, string | conform.FiletypeFormatter> Formatters. `string` for single filetype, `conform.FiletypeFormatter` formultiple filetypes with optional conform options, or `table` for explicit filetype mapping.
+--- @field formatter? string | conform.FiletypeFormatter | table<string, string | conform.FiletypeFormatter> Formatters. `string` for single filetype, `conform.FiletypeFormatter` for multiple filetypes with optional conform options, or `table` for explicit filetype mapping.
 --- @field linter? string | string[] | table<string, string|string[]> Linters. `string` uses lang_name, `string[]` specifies multiple linter tools for lang_name filetype, or `table` for explicit filetype mapping.
 --- @field dap? table DAP (Debug Adapter Protocol) configuration
 --- @field plugin? LazyPluginSpec | LazyPluginSpec[] Extra plugin(s) to install. Can be a single plugin spec or a list of plugin specs.
@@ -17,7 +17,7 @@
 --- @field filetype string | string[] Filetype(s) associated with the language.
 --- @field mason? MasonPackageSpec | MasonPackageSpec[] Mason packages to install. Can be a single package name/spec or a list of package names/specs.
 --- @field lsp? string | table<string, vim.lsp.ClientConfig> LSP servers configuration. Can be a single server name (default config) or (server_name -> config) map.
---- @field formatter? string | string[] | table<string, string|string[]> Formatters. `string` uses tool_name, `string[]` specifies multiple filetypes that tool_name formats, or `table` for explicit filetype mapping.
+--- @field formatter? true | string | string[] | table<string, string|string[]> Formatters. `true` uses tool_name, `string` uses tool, `string[]` specifies multiple filetypes that tool_name formats, or `table` for explicit filetype mapping.
 --- @field linter? string | string[] | table<string, string | string[]> Linters. `string` uses tool_name, `string[]` specifies multiple filetypes that tool_name lints, or `table` for explicit filetype mapping.
 --- @field plugin? LazyPluginSpec | LazyPluginSpec[] Extra plugin(s) to install. Can be a single plugin spec or a list of plugin specs.
 
