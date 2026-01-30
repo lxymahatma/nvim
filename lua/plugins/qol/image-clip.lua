660f0618693a844d9098a1c3d9b65697457cd1c9
lxymahatma <lxymahatma@gmail.com>
2026-01-30 16:58:31 +0800

    lazy load img-clip

--- lua/plugins/qol/image-clip.lua
+++ lua/plugins/qol/image-clip.lua
@@ -1,7 +1,6 @@
 --- @type LazyPluginSpec
 return {
     "HakonHarnes/img-clip.nvim",
-    event = "BufEdit",
     opts = {},
     keys = {
         { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image" },
