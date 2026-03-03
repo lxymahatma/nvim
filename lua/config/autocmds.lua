e20cdbf8e5bd6f21b498a62b9d71815e51486947
lxymahatma <lxymahatma@gmail.com>
2026-03-03 23:03:14 +0800

    add indentexpr from treesitter

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -37,6 +37,7 @@ vim.api.nvim_create_autocmd("FileType", {
 
         vim.wo.foldmethod = "expr"
         vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 
         local ft_config = lang_parser.get_config_by_ft(args.match)
 
