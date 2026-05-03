eed0a5adb5a784612aac585498f5a7f6f0b2200c
lxymahatma <lxymahatma@gmail.com>
2026-05-03 21:24:34 +0900

    fixing format

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,36 +1,32 @@
 ---@type LazyPluginSpec[]
 return {
-    -- Mason
-    {
-        "mason-org/mason.nvim",
-        event = "VeryLazy",
-        cmd = "Mason",
+    "mason-org/mason.nvim",
+    event = "VeryLazy",
+    cmd = "Mason",
 
-        ---@type MasonSettings
-        opts = {
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
-            keymaps = {
-                toggle_package_expand = "l",
-                toggle_package_install_log = "l",
+    ---@type MasonSettings
+    opts = {
+        ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗",
             },
         },
-        config = function(_, opts)
-            require("mason").setup(opts)
-
-            vim.defer_fn(function()
-                local packages = require("toolchain").get_mason_packages()
-                require("helpers.mason").ensure_packages_installed(packages)
-            end, 100)
-        end,
-        keys = {
-            { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" },
+        keymaps = {
+            toggle_package_expand = "l",
+            toggle_package_install_log = "l",
         },
     },
+    config = function(_, opts)
+        require("mason").setup(opts)
 
+        vim.defer_fn(function()
+            local packages = require("toolchain").get_mason_packages()
+            require("helpers.mason").ensure_packages_installed(packages)
+        end, 100)
+    end,
+    keys = {
+        { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" },
+    },
 }
