b7cb55658253e1a2bc5412cdff7560d51c22d827
lxymahatma <lxymahatma@gmail.com>
2026-09-24 23:35:14 +0900

    fix colorizer names option key

--- lua/plugins/highlight/colorizer.lua
+++ lua/plugins/highlight/colorizer.lua
@@ -57,7 +57,7 @@ return {
     options = {
       parsers = {
         names = {
-          enabled = false,
+          enable = false,
           lowercase = false,
           camelcase = false,
         },
