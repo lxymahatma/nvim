95f56a36de001c4e3a622fe94b83bf00f6c3957e
lxymahatma <lxymahatma@gmail.com>
2026-09-25 20:45:29 +0900

    drop remap from cmd and function keymaps

--- lua/config/keymaps/buffer.lua
+++ lua/config/keymaps/buffer.lua
@@ -1,8 +1,8 @@
 local map = Snacks.keymap.set
 
-map("n", "H", "<cmd>TabScopePrev<cr>", { desc = "Previous Buffer", remap = true })
-map("n", "L", "<cmd>TabScopeNext<cr>", { desc = "Next Buffer", remap = true })
-map("n", "<leader>bd", Snacks.bufdelete.delete, { desc = "Delete current buffer", remap = true })
-map("n", "<leader>bo", Snacks.bufdelete.other, { desc = "Delete other buffers", remap = true })
-map("n", "<leader>bl", "<cmd>TabScopeCloseLeft<cr>", { desc = "Close buffers on the left", remap = true })
-map("n", "<leader>br", "<cmd>TabScopeCloseRight<cr>", { desc = "Close buffers on the right", remap = true })
+map("n", "H", "<cmd>TabScopePrev<cr>", { desc = "Previous Buffer" })
+map("n", "L", "<cmd>TabScopeNext<cr>", { desc = "Next Buffer" })
+map("n", "<leader>bd", Snacks.bufdelete.delete, { desc = "Delete current buffer" })
+map("n", "<leader>bo", Snacks.bufdelete.other, { desc = "Delete other buffers" })
+map("n", "<leader>bl", "<cmd>TabScopeCloseLeft<cr>", { desc = "Close buffers on the left" })
+map("n", "<leader>br", "<cmd>TabScopeCloseRight<cr>", { desc = "Close buffers on the right" })
--- lua/config/keymaps/tab.lua
+++ lua/config/keymaps/tab.lua
@@ -1,8 +1,8 @@
 local map = Snacks.keymap.set
 
-map("n", "[t", "<cmd>tabprevious<cr>", { desc = "Previous Tab", remap = true })
-map("n", "]t", "<cmd>tabnext<cr>", { desc = "Next Tab", remap = true })
-map("n", "[T", "<cmd>tabfirst<cr>", { desc = "First Tab", remap = true })
-map("n", "]T", "<cmd>tablast<cr>", { desc = "Last Tab", remap = true })
-map("n", "<leader>tn", "<cmd>tab split<cr>", { desc = "New tab with current file", remap = true })
-map("n", "<leader>tc", "<cmd>tabclose<cr>", { desc = "Close Tab", remap = true })
+map("n", "[t", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+map("n", "]t", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "[T", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+map("n", "]T", "<cmd>tablast<cr>", { desc = "Last Tab" })
+map("n", "<leader>tn", "<cmd>tab split<cr>", { desc = "New tab with current file" })
+map("n", "<leader>tc", "<cmd>tabclose<cr>", { desc = "Close Tab" })
