8379bdcc7b376cbec64ddf173f564f846b321015
lxymahatma <lxymahatma@gmail.com>
2026-09-25 20:45:23 +0900

    remove unused treesitter helper functions

--- lua/helpers/treesitter.lua
+++ lua/helpers/treesitter.lua
@@ -5,16 +5,4 @@ local M = {}
 ---@param parser_list string[] List of parser names to ensure are installed
 M.ensure_parsers_installed = function(parser_list) ts.install(parser_list) end
 
----@param parser_name string
-M.install_parser = function(parser_name)
-  vim.notify(("[treesitter] Installing parser: %s"):format(parser_name), vim.log.levels.INFO)
-  ts.install(parser_name, { force = false, summary = true })
-end
-
----@param parser_name string
-M.update_parser = function(parser_name)
-  vim.notify(("[treesitter] Updating parser: %s"):format(parser_name), vim.log.levels.INFO)
-  ts.update(parser_name, { summary = false })
-end
-
 return M
